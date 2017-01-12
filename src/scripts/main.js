9c92ebf00ae88611ce995dc298100e4b7780977c
Moninets <mninec@ukr.net>
2017-01-12 20:19:53 +0200

    initial commit

---
+++ src/scripts/main.js
@@ -0,0 +1,16 @@
+;(function($) {
+	"use strict";
+	$(function(){
+	$('.ba-slider').slick({
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		// dots: true,
+		infinite: true,
+		// autoplay: true,
+		// autoplaySpeed: 10000,
+		// arrows: false
+	
+	})
+
+	});
+})(jQuery);
